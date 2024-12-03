663cf1e73acca0a05d15c19aca4456aba59fa297
Ahmed Dikha <85714557+bestwall2@users.noreply.github.com>
2024-12-03 14:03:36 +0100

    Update page.tsx

--- app/anime/info/[id]/page.tsx
+++ app/anime/info/[id]/page.tsx
@@ -2,14 +2,14 @@
 
 import { useRouter } from 'next/router';
 
-export default function AnimeInfoLayout({ children }: { children: React.ReactNode }) {
+export default function AnimeInfoLayout() {
   const router = useRouter();
   const { id } = router.query;
 
   return (
     <>
       <h1>Anime ID: {id}</h1>
-      {children}
+     
     </>
   );
 }
